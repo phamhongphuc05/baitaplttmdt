8470bb7a665a8c1fe6abb53bb18259c5f18d79be
phamhongphuc05 <phamhongphuc2021@gmail.com>
2024-08-19 21:41:52 +0700

    Add files via upload

---
+++ 2chuso.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 19:11:28 2024
+
+@author: Phạm Hồng Phúc 23706461 
+"""
+a= int(input("Nhập số hàng chục:"))
+b=int(input("Nhập số hàng đơn vị:"))
+if 0<a<=9:
+    print ("Tổng các chữ số là",(a+b))
+else:
+    print("Không hợp lệ")
+
+
+
